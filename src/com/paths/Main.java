9cd46b7dd78e88988f9a0ad1d14364594eef1d67
Penny <sonaralee@users.noreply.github.com>
2018-06-08 18:15:30 -0500

    finish paths (for now)

--- src/com/paths/Main.java
+++ src/com/paths/Main.java
@@ -11,24 +11,45 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Main {
 
     public static void main(String[] args) {
-        Path path = FileSystems.getDefault()
-                            .getPath("WorkingDirectory.txt");
-        printFile(path);
 
-        Path subPath = FileSystems.getDefault()
-                .getPath("res", "Subdirectory.txt");
-        printFile(subPath);
+        try {
+//            Path sourceFile = FileSystems.getDefault().getPath("res","data.txt");
+//            Path copyFile = FileSystems.getDefault().getPath("res", "data_copy.txt");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+//
+//            sourceFile = FileSystems.getDefault().getPath("res");
+//            copyFile = FileSystems.getDefault().getPath("rescopy");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+            Path file = FileSystems.getDefault().getPath("res","data.txt");
+            BasicFileAttributes attrs = Files.readAttributes(file, BasicFileAttributes.class);
+            System.out.println((attrs.size()));
+            Object i;
 
-        Path outPath = Paths.get("C:\\Users\\annal_000\\IdeaProjects\\OutThere.txt");
-        printFile(outPath);
+        } catch(IOException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
 
-        subPath = Paths.get(".");
-        System.out.println(subPath.toAbsolutePath());
+
+
+//        Path path = FileSystems.getDefault()
+//                            .getPath("WorkingDirectory.txt");
+//        printFile(path);
+//
+//        // Path filePath = FileSystems.getDefault().getPath("res", "Subdirectory.txt");
+//        Path filePath = Paths.get(".","res", "Subdirectory.txt");
+//        printFile(filePath);
+//
+//        // filePath = Paths.get("C:\\Users\\annal_000\\IdeaProjects\\OutThere.txt");
+//        filePath = Paths.get("C:\\", "Users\\annal_000\\IdeaProjects", "OutThere.txt");
+//        printFile(filePath);
+//
+//        filePath = Paths.get(".");
+//        System.out.println(filePath.toAbsolutePath());
     }
 
     private static void printFile(Path path) {
